bb03bce63e1573a3d9f2e79df56d3f5441194a18
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-06-04 15:57:44 -0400

    Feat: Agregar cambio de contrasena

--- modulos/authentication.py
+++ modulos/authentication.py
@@ -1,23 +1,24 @@
+import os
+
 from cryptography.fernet import Fernet
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 from mongo import *
 
-config = dotenv_values(".env")
+load_dotenv()
 
 def register(credentials):
-    fernet = Fernet(config['KEY_SECRET'])
+    fernet = Fernet(os.getenv('KEY_SECRET'))
     new_credentials = credentials.copy()
     new_credentials['password'] = fernet.encrypt(new_credentials['password'].encode()).decode()
-    return create("users", new_credentials)
+    user_id = create("users", new_credentials)
+    return get("users", user_id)
 
 def login(credentials):
-    fernet = Fernet(config['KEY_SECRET'])
+    fernet = Fernet(os.getenv('KEY_SECRET'))
     user = list(getAll("users", {"username": credentials['username']}))
 
     if len(user) != 0:
-        print(credentials['password'])
         decrypted_password = fernet.decrypt(user[0]['password'].encode()).decode()
-        print(decrypted_password)
         if decrypted_password == credentials['password']:
             user[0]['password'] = decrypted_password
             return user[0]
@@ -25,3 +26,8 @@ def login(credentials):
             return "Nombre de Usuario o contraseña incorrecta"
     else:
         return "Nombre de Usuario o contraseña incorrecta"
+
+def change_password(user_id, new_password):
+    fernet = Fernet(os.getenv('KEY_SECRET'))
+    encripted_password = fernet.encrypt(new_password.encode()).decode()
+    update('users', user_id, {'password': encripted_password})
